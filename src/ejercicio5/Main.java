ddc41126fd5bf0fc2dc05a4ff8072298a06ece45
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-12-14 14:10:57 +0100

    No consigo invertirlo

---
+++ src/ejercicio5/Main.java
@@ -0,0 +1,39 @@
+package ejercicio5;
+
+// ? Importamos la Clase Scanner y Locale
+import java.util.Scanner;
+import java.util.Locale;
+
+// ? Importamos la Clase Arrays y Collections
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        // ^ Definimos un array entero de 8 posiciones
+        int calificaciones[] = new int[8];
+
+        // ^ Definimos una instancia de la clase Scanner de tipo final de nombre read y cambiamos su formato a US
+        final Scanner read = new Scanner(System.in);
+        read.useLocale(Locale.US);
+
+        // ! Pedimos al Usuario los valores de los jugadores
+        System.out.println("Introduce la calificación de los 8 jugadores:");
+        for (byte pos = 0; pos < calificaciones.length; pos++) {
+            System.out.print("[" + pos + "] > ");
+            calificaciones[pos] = read.nextInt();
+        }
+
+        // ^ Cerramos el Scanner tras su uso
+        read.close();
+
+        // ! Ordenamos los valores del Array de forma ascendente
+        Arrays.sort(calificaciones);
+
+        // ! Invertimos el orden del Array
+        
+
+        // ! Mostramos el Array
+        System.out.println("Calificaciones Ordenadas de Mayor a menor:\n" + Arrays.toString(calificaciones));
+    }
+}
